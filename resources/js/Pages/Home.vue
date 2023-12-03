f0999188719aa1631353802667406ca88c904f6b
KNR1997 <kethakaranasinghe@gmail.com>
2023-12-03 23:31:13 +0530

    First commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <!-- <div class="bg-green-500">Home.VUE</div> -->
+
+    </MainLayout>
+
+</template>
